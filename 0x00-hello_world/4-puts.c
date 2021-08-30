ec2bde994c61ddf7253c479edf3cf87ab0456b3f
Damiano Semufali <damiansemufali@gmail.com>
2021-08-30 16:13:35 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+intmain (void)
+{
+	puts("Programming is like a building a multilingual puzzle");
+	return (0);
+}
+
