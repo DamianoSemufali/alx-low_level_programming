2b9d166bf733263ad9a687e0bef309fd3c1bd087
Damiano Semufali <damiansemufali@gmail.com>
2021-08-30 16:46:15 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,7 +10,6 @@ int main(void)
 
 	char displayed[52];
 
-	strcpy(displayed, "\"Programming is like a building a multilingual puzzle");
-	puts(displayed);
+	puts("\"Programming is like a building a multilingual puzzle");
 	return (0);
 }
