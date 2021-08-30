1a2aeb949f8fadcbc83fb31b02ed8111bf93b919
Damiano Semufali <damiansemufali@gmail.com>
2021-08-30 16:38:02 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
-#!/bin/bash
 #include <stdio.h>
+#include <string.h>
+
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Return: return 0 everytime
  */
-
-intmain (void)
+int main(void)
 {
-	puts("Programming is like a building a multilingual puzzle");
+
+	char displayed[52];
+
+	strcpy(displayed, "\"Programming is like a building a multilingual puzzle");
+	puts(displayed);
 	return (0);
 }
-
