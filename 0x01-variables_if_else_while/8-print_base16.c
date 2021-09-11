91be59664692f6137cff9ca901d088144d215a2e
Damiano Semufali <damiansemufali@gmail.com>
2021-09-11 03:41:09 -0700

    Base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- *main - using putchar to print hexadecimal base 
- *Return: Always 0 (Success)
+ * main - using putchar, print hexadecimal base
+ * Return: Always 0 (Success)
  */
 
 int main(void)
