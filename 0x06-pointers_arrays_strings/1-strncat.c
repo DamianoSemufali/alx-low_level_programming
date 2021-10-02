e2ec72727aa225a095d8d3bb87c3a1d0788314af
Damiano Semufali <damiansemufali@gmail.com>
2021-10-01 23:44:06 -0700

    StrnCat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,12 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * _strncat - concatenate n bytes to destination string
- *
- *   * @dest: string to be appended to
- *
- *    * @src: string to append
- *
- *     * @n: append n number of bytes(chars)
- *
- *      * Return: concatenated string
- *
- *       */
-
-
+ * _strncat - concatenate n bytes to destination string
+ * @dest: string to be appended to
+ * @src: string to append
+ * @n: append n number of bytes(chars)
+ * Return: concatenated string
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
@@ -24,27 +14,13 @@ char *_strncat(char *dest, char *src, int n)
 	int j = 0;
 
 	while (dest[i] != '\0') /*i is index of null terminator*/
-
-							i++;
-
-
-
-					while (src[j] != src[n]) /*append replacing null terminator*/
-
-							{
-
-										dest[i] = src[j];
-
-												i++;
-
-														j++;
-
-															}
-
-						dest[i] = '\0';
-
-
-
-							return (dest);
-
+		i++;
+	while (src[j] != src[n]) /*append replacing null terminator*/
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
